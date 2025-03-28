7f9977213237b9d308f512f12323af21ccd9479c
reinerito <reiner.stieger@gmail.com>
2025-03-28 11:45:31 +0100

    finished challenge

---
+++ react-state-1_box/src/App.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+export default function App() {
+  const [isActive, setisActive] = useState(false);
+
+  function handleClick() {
+    setisActive(!isActive);
+    // Check that the value changes correctly.
+    console.log(isActive);
+  }
+
+  return (
+    <main>
+      <div className={`box ${isActive ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        {isActive ? "Is Deactivated" : "Activate"}
+      </button>
+    </main>
+  );
+}
